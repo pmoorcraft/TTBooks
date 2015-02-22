bc1bea88eae83c957b74e2d13e1e9e32b82f22d3
pmoorcraft <pedro.moorcraft@gmail.com>
2015-02-22 17:31:46 +0000

    Inital migration from static HTML site

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'homepage#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   #
   #     collection do
   #       get 'sold'
-  #     end
+  #     
   #   end
 
   # Example resource route with sub-resources:
