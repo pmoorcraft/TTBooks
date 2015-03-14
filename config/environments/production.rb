4640388dc4c78565d46905cdad755c4f8e5788fb
pmoorcraft <pedro.moorcraft@gmail.com>
2015-03-14 22:52:40 +0000

    Added Font Awesome gem and redid footer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg *.ico *.eot *.ttf)
+  
   Paperclip.options[:command_path] = "/opt/local/bin/convert"
 end
