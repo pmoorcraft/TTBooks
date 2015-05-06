16c8532d837fcdf37b13312ad43521ab566791fc
pmoorcraft <pedro.moorcraft@gmail.com>
2015-05-06 23:51:48 +0100

    experimenting with asset precompile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.assets.compile = false
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   # config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg *.ico *.eot *.ttf)
