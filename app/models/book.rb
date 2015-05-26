7397dba628fd584d97314be54ed185d746cefa74
pmoorcraft <pedro.moorcraft@gmail.com>
2015-05-26 21:55:08 +0100

    trying to fix avatar issue

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :avatar, :styles => { :medium => "150x200>", :thumb => "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_many :votes, dependent: :destroy
   
